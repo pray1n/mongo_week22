dc852d7cabe477f3be49552b858e7883a95054c7
pray1n <89249367+pray1n@users.noreply.github.com>
2022-09-14 19:28:05 +0200

    rf

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const Student = require('./model/student')
 
 const mongodbPassword = 'ZhNqoPdAg5ERJBMa'
-const mongodbConnection = `mongodb+srv://justinw:${mongodbPassword}@cluster0.zxpk1sy.mongodb.net/?retryWrites=true&w=majority`
+const mongodbConnection = `mongodb+srv://justinw:${mongodbPassword}@cluster0.6cz2olv.mongodb.net/?retryWrites=true&w=majority`
+
+
 
 mongoose.connect(mongodbConnection)
 
@@ -18,10 +20,38 @@ app.use(express.json())
 
 const port = 3000
 
-app.post('/student', (req, res) => {
+function _makeApiStudent(dbStudent) {
+    return {
+        id: dbStudent._id,
+        name: dbStudent.name,
+        firstName: dbStudent.first_name,
+        email: dbStudent.email
+    }
+}
+
+app.post('/student', async (req, res) => {
     console.log(req.body)
-    res.send({created: req.body})
+    const newStudent = await Student.create({
+        name: req.body.name,
+        first_name: req.body.firstName,
+        email: req.body.email
+    })
+
+    res.send(newStudent)
+
+    console.log('Creation in')
 })
 
+app.get('/student', async (req, res) => {
+    const studentsArray = await Student.find({})
+        res.send(studentsArray.map((e) => _makeApiStudent(e)
+    ))
+})
+
+app.get('/student/:id', async (req, res) => {
+    const {id} = req.params
+    const e = await Student.findById(id)
+    res.send(_makeApiStudent(e))
+})
 
 app.listen(port, () => console.log('Server listening at ' + port))
